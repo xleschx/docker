54998ddf30917893a7c6a1114a98990d607291c5
lsc132428 <lsc132428@stud.gibb.ch>
2023-02-23 13:02:42 +0100

    test

--- zytroy-demo/app.py
+++ zytroy-demo/app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+def home():
+    return f"""<html>
+                <head>
+                    <title>Demo for Docker</title>
+                </head>
+                <body>
+                  <h1>Welcome to this demo for Docker</h1>
+                    <h2>This is a project for school purposes</h2>
+                    <p>Test1</p>
+                </body>
+            </html>"""
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
